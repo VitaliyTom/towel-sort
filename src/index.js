a9d519d76cf0583793593632783454f9e842cef7
VitaliyTom <tomashevich.vitaliy88@gmail.com>
2020-03-09 14:09:23 +0300

    refactor: change structure of the project

--- src/index.js
+++ src/index.js
@@ -3,21 +3,6 @@ module.exports = function towelSort(matrix) {
 	if (typeof matrix === 'undefined') {
 		return array;
 	}
-	for (let i = 0; i < matrix.length; i++) {
-		let arrayReverse = [];
-		for (let j = 0; j < matrix[i].length; j++) {
-			arrayReverse.push(matrix[i][j]);
-		}
-		if (i % 2 !== 0) {
-			arrayReverse.reverse();
-			for (let k = 0; k < arrayReverse.length; k++) {
-				array.push(arrayReverse[k]);
-			}
-		} else {
-			for (let l = 0; l < arrayReverse.length; l++) {
-				array.push(arrayReverse[l]);
-			}
-		}
-	}
+	array = matrix.map((array, index) => (index % 2 !== 0 ? array.reverse() : array)).flat();
 	return array;
 };
